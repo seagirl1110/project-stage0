59fbef85daf935e703ec0224a0a68953d9ecaf39
Marina Elizarova <seagirl1110@gmail.com>
2022-01-19 13:54:54 +0300

    feat: add change portfolio seasonal image

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,5 @@
+// hamburger-menu
+
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
 
@@ -13,6 +15,24 @@ function closeMenu(event) {
     if (event.target.classList.contains('menu__link')) {
         hamburgerMenu.classList.remove('hamburger-menu--open');
         menu.classList.remove('menu--open');
+    }
+}
+
+
+// portfolio-seasonal-images
+
+const portfolioBtnWrapper = document.querySelector('.portfolio__btn-wrapper');
+const portfolioBtns = document.querySelectorAll('.portfolio-btn');
+const portfolioImages = document.querySelectorAll('.portfolio__img');
+
+portfolioBtnWrapper.addEventListener('click', changeImage);
+
+function changeImage(event) {
+    if (event.target.classList.contains('portfolio-btn')) {
+        portfolioBtns.forEach((btn) => btn.classList.remove('portfolio-btn--active'));
+        event.target.classList.add('portfolio-btn--active');
+        const season = event.target.dataset.season;
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
     }
 }
 
