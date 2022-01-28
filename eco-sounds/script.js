49f967e15e0c42ca0c278f2da092f1c8dcfbaa9d
Marina Elizarova <seagirl1110@gmail.com>
2022-01-28 15:51:05 +0300

    feat: add  audio

--- eco-sounds/script.js
+++ eco-sounds/script.js
@@ -12,3 +12,34 @@ function changeClassActive(event) {
         element.classList.add('nav-item--active');
     }
 }
+
+// audio
+
+const audio = new Audio();
+const audioBtn = document.querySelector('[data-js="audio-btn"]');
+let isPlay = false;
+
+
+audioBtn.addEventListener('click', (event) => {
+    const element = event.target;
+    toggleBtn(element);
+    playAudio();
+});
+
+function toggleBtn(element) {
+    element.classList.toggle('btn-play--pause');
+}
+
+function playAudio() {
+   
+    audio.src = './assets/audio/forest.mp3';
+    audio.currentTime = 0;
+    if (!isPlay) {
+        audio.play();
+        isPlay = true;
+
+    } else {
+        audio.pause();
+        isPlay = false;
+    }
+}
