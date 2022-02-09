9435ac49d5777cee50af05a34ee9678580a4c378
Marina Elizarova <seagirl1110@gmail.com>
2022-02-09 08:58:28 +0300

    feat: add enter

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,16 +1,21 @@
-getResult();
+const search = document.querySelector('[data-js="search"]');
 const searchBtn = document.querySelector('[data-js="search-btn"]');
+
+search.addEventListener('keydown', (key) => {
+    if (key.keyCode === 13) {getResult()}
+})
 searchBtn.addEventListener('click', getResult);
+
+getResult();
 
 async function getResult() {
     const result = await getData();
     const collImages = result.results;
     const galleryContainer = document.querySelector('[data-js="gallery"]');
-    return addImage(collImages, galleryContainer);
+    addImage(collImages, galleryContainer);
 }
 
 async function getData() {
-    const search = document.querySelector('[data-js="search"]');
     const url = getUrl(search.value);
     const res = await fetch(url);
     const data = await res.json();
@@ -19,9 +24,9 @@ async function getData() {
 
 function getUrl(value) {
     if (value.length > 0) {
-        url = `https://api.unsplash.com/search/photos?query=${value}&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
+        url = `https://api.unsplash.com/search/photos?query=${value}&per_page=12&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
     } else {
-        url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+        url = 'https://api.unsplash.com/search/photos?query=spring&per_page=12&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
     }
     return url;
 }
