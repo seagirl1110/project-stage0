dda65c2a86aa2359eafa22e28c1816b17139d1f8
Marina Elizarova <seagirl1110@gmail.com>
2022-02-17 11:08:20 +0300

    fix: fix card, fix flipCard

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,10 +1,36 @@
 const cards = document.querySelectorAll('[data-js="card"]');
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
 
-function flipCard(evt) {   
+function flipCard(evt) {
     const elem = evt.currentTarget;
     elem.classList.add('card--flip');
+    if (!hasFlippedCard) {
+        firstCard = elem;
+        hasFlippedCard = true;
+    } else {
+        secondCard = elem;
+        hasFlippedCard = false;
+        checkForMatch()
+    }
+}
+
+function checkForMatch() {
+    console.log(firstCard.dataset.animal);
+    if (firstCard.dataset.animal !== secondCard.dataset.animal) {
+        console.log(secondCard.dataset.animal);
+        unflipCards()
+    }
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('card--flip');
+        secondCard.classList.remove('card--flip');
+    }, 1500)
 }
